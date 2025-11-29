f517923968b8f69623b91216dc85c7f884529105
rajathshttgr <rajathshettigar.uy@gmail.com>
2025-11-29 21:32:07 +0530

    add redis url case

--- backend/app/core/redis.py
+++ backend/app/core/redis.py
@@ -1,12 +1,15 @@
+import os
 import redis.asyncio as redis
 from app.core.config import settings
 
-REDIS_URL = f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/0"
+if settings.REDIS_URL:
+    REDIS_URL = settings.REDIS_URL
+else:
+    REDIS_URL = f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/0"
 
 pool = redis.ConnectionPool.from_url(REDIS_URL, decode_responses=True)
 
 async def get_redis():
-
     client = redis.Redis(connection_pool=pool)
     try:
         yield client
